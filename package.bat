a1c94b808befc435c9ae82d867baaf09804f286a
Even Torset <eventorset@gmail.com>
2020-04-11 21:36:44 +0200

    Initial commit.

---
+++ package.bat
@@ -0,0 +1,4 @@
+SET "gameDir=C:\Program Files (x86)\Minecraft Dungeons Launcher\products\dungeons\dungeons"
+SET "paksPath=Dungeons\Content\Paks"
+
+python Tools\u4pak.py pack "%gameDir%\%paksPath%\z_Dokucraft-Dungeons.pak" Dungeons
