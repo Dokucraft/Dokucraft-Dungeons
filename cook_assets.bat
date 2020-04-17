a480b127ffacd31ca3ed9784e812e4a94009ec67
WantedRobot <M.J.Murphy@live.co.uk>
2020-04-17 20:23:48 +0100

    Create cook_assets.bat

---
+++ cook_assets.bat
@@ -0,0 +1,12 @@
+SET "ddp=%~dp0"
+SET "ddp=%ddp:~0,-1%"
+
+SET "editorPath=C:\Program Files\Epic Games\UE_4.22\Engine\Binaries\Win64"
+
+del /S Dungeons\*.uasset
+del /S Dungeons\*.ubulk
+del /S Dungeons\*.uexp
+
+"%editorPath%\UE4Editor-Cmd.exe" "%ddp%\UE4Project\Dungeons.uproject" -run=cook -targetplatform=WindowsNoEditor
+
+robocopy /job:Tools\copy_cooked_assets
